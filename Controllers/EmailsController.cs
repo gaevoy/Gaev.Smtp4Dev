00ee1cd9d3ffa8c3349e07ac4a6febd019ccc9ce
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2020-03-20 12:52:49 +0100

    Log improvements

--- Controllers/EmailsController.cs
+++ Controllers/EmailsController.cs
@@ -5,14 +5,25 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Serilog.Core;
 
 namespace Gaev.Smtp4Dev.Controllers
 {
     [Route("api/emails")]
     public class EmailsController : ControllerBase
     {
-        private static readonly ConcurrentDictionary<string, List<StreamWriter>> Inboxes =
-            new ConcurrentDictionary<string, List<StreamWriter>>();
+        private static readonly ConcurrentDictionary<string, List<ClientHandle>> Inboxes =
+            new ConcurrentDictionary<string, List<ClientHandle>>();
+
+        public class ClientHandle
+        {
+            public string Ip { get; set; }
+            public string Ip2 { get; set; }
+            public int Port { get; set; }
+            public StreamWriter Response { get; set; }
+        }
+
+        private static Logger Logger => Program.Logger;
 
         // GET api/emails/test@smtp4dev.gaevoy.com
         [HttpGet("{recipientEmail}")]
@@ -21,37 +32,47 @@ namespace Gaev.Smtp4Dev.Controllers
             Response.Headers["Cache-Control"] = "no-cache"; // https://serverfault.com/a/801629
             Response.Headers["X-Accel-Buffering"] = "no";
             Response.ContentType = "text/event-stream";
-            await using var member = new StreamWriter(Response.Body);
-            var listeners = Inboxes.GetOrAdd(recipientEmail.ToLower(), _ => new List<StreamWriter>(4));
-            lock (listeners)
-                listeners.Add(member);
-            await member.WriteAsync("event: connected\ndata:\n\n");
-            await member.FlushAsync();
+            await using var response = new StreamWriter(Response.Body);
+            var clients = Inboxes.GetOrAdd(recipientEmail.ToLower(), _ => new List<ClientHandle>(4));
+            var client = new ClientHandle
+            {
+                Ip = Request.Headers["X-Real-IP"].ToString(),
+                Port = HttpContext.Connection.RemotePort,
+                Response = response
+            };
+            lock (clients)
+                clients.Add(client);
+            await response.WriteAsync("event: connected\ndata:\n\n");
+            await response.FlushAsync();
+            Logger.Information("Client connected {@cli}", new {client.Ip, client.Port});
             await HttpContext.RequestAborted.AsTask();
-            lock (listeners)
-                listeners.Remove(member);
+            lock (clients)
+                clients.Remove(client);
+            Logger.Information("Client disconnected {@cli}", new {client.Ip, client.Port});
         }
 
-        public static async Task OnMessageReceived(string recipientEmail, string messageInJson)
+        public static async Task OnMessageReceived(string recipientEmail, string messageInJson, Guid messageId)
         {
-            List<StreamWriter> SafeCopy(List<StreamWriter> streamWriters)
+            List<ClientHandle> SafeCopy(List<ClientHandle> streamWriters)
             {
                 lock (streamWriters)
                     return streamWriters.ToList();
             }
 
-            if (!Inboxes.TryGetValue(recipientEmail.ToLower(), out var listeners))
+            if (!Inboxes.TryGetValue(recipientEmail.ToLower(), out var clients))
                 return;
-            foreach (var listener in SafeCopy(listeners))
+            foreach (var client in SafeCopy(clients))
                 try
                 {
-                    await listener.WriteAsync("data: " + messageInJson + "\n\n");
-                    await listener.FlushAsync();
+                    await client.Response.WriteAsync("data: " + messageInJson + "\n\n");
+                    await client.Response.FlushAsync();
+                    Logger.Information("Message sent {@cli}", new {client.Ip, client.Port, messageId});
                 }
                 catch (ObjectDisposedException)
                 {
-                    lock (listeners)
-                        listeners.Remove(listener);
+                    lock (clients)
+                        clients.Remove(client);
+                    Logger.Information("Client is disposed {@cli}", new {client.Ip, client.Port, messageId});
                 }
         }
     }
--- Program.cs
+++ Program.cs
@@ -5,53 +5,75 @@ using System.Threading.Tasks;
 using Gaev.Smtp4Dev.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using netDumbster.smtp;
+using Serilog;
+using Serilog.Core;
 
 namespace Gaev.Smtp4Dev
 {
-    class Program
+    public class Program
     {
+        public static Logger Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.RollingFile("log-{Date}.txt")
+            .CreateLogger();
+
         static async Task Main(string[] args)
         {
             var cancellation = new CancellationTokenSource();
             Console.CancelKeyPress += (__, e) =>
             {
+                Logger.Information("Stopping");
                 e.Cancel = true;
                 cancellation.Cancel();
             };
 
-            using var server = SimpleSmtpServer.Start(25);
+            using var server = Configuration.Configure().WithPort(25).Build();
             server.MessageReceived += (__, receivedArgs) =>
                 Task.Run(() =>
                     OnMessageReceivedAsync(receivedArgs.Message));
-            _ = Task.Factory.StartNew(async () =>
+            Logger.Information("Started");
+            try
             {
-                while (true)
-                {
-                    await Task.Delay(60_000, cancellation.Token);
-                    server.ClearReceivedEmail();
-                }
-            });
-            await Host
-                .CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-                .Build()
-                .RunAsync(cancellation.Token);
+                await Host
+                    .CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                    .Build()
+                    .RunAsync(cancellation.Token);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Stopping");
+            }
+            finally
+            {
+                Logger.Information("Stopped");
+                Logger.Dispose();
+            }
         }
 
         static async Task OnMessageReceivedAsync(SmtpMessage message)
         {
+            var messageId = Guid.NewGuid();
+            Logger.Information("Message received {@msg}", new {messageId});
             var messageInJson = JsonSerializer.Serialize(message.Data);
             foreach (var recipient in message.ToAddresses)
-                await EmailsController.OnMessageReceived(recipient.Address, messageInJson);
+                await EmailsController.OnMessageReceived(recipient.Address, messageInJson, messageId);
         }
 
         class Startup
         {
-            public void ConfigureServices(IServiceCollection services) =>
+            public void ConfigureServices(IServiceCollection services)
+            {
                 services.AddMvc();
+                services.Configure<ForwardedHeadersOptions>(options =>
+                {
+                    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor;
+                });
+            }
 
             public void Configure(IApplicationBuilder app, IWebHostEnvironment env) =>
                 app
